e4d3d64c3bd097cb0fe6ab6b5e9920ad24b21181
mxnoob <noobmx@gmail.com>
2024-05-07 19:53:40 +0300

    [add] about/technologies

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/mxnoob/foodgram" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/mxnoob" className={styles.textLink}>Имя Автора</a>
             </p>
           </div>
         </aside>
