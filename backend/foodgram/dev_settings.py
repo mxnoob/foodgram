118301771623458d4495f25be341720f865936bb
mxnoob <noobmx@gmail.com>
2024-05-08 12:12:30 +0300

    [update] settings для тестов

---
+++ backend/foodgram/dev_settings.py
@@ -0,0 +1,30 @@
+# flake8: noqa: E501,F403,F405
+import os
+
+from .settings import *
+
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+ROOT_URLCONF = 'foodgram.dev_urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+if not os.getenv('IS_TEST', '').lower() in ('true', '1'):
+    REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
+        'rest_framework.authentication.SessionAuthentication',
+    )
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda r: True,
+}
